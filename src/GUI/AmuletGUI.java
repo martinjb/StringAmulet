1e617ef80c94950d48cd3436a28d7c9a12cf3e7b
martinjb <martinjb@appstate.edu>
2018-07-25 04:02:57 -0400

    GUI Changes
    Added interface for GUI
    
    GUI now organized as a frame, one button, a price panel and a result panel.

--- src/GUI/AmuletGUI.java
+++ src/GUI/AmuletGUI.java
@@ -1,61 +1,104 @@
 package GUI;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JWindow;        
 
-public class AmuletGUI {
+public class AmuletGUI implements intAmuletGUI {
 	    private static void createAndShowGUI() {
 	        //Create and set up the window.
+	    	float x = 0.0f;
+	    	float y = 1.0f;
+	    	float z = 0.4f;
+
 	        JFrame entryContainer = new JFrame("This is the Window Title");
-	        entryContainer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	        
+	        entryContainer.getContentPane().setBackground(new Color(x,y,z));
+	        entryContainer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        entryContainer.setLayout(new GridLayout(2, 2));
+	        
+	        JPanel pricePanel = new JPanel(new FlowLayout());
+	        JPanel resultPanel = new JPanel(new FlowLayout());	        
+	        JPanel priceSPanel = new JPanel(new FlowLayout());
+	        JPanel priceAPanel = new JPanel(new FlowLayout());
+	        JPanel priceUPanel = new JPanel(new FlowLayout());	        
+	        JPanel resultMPanel = new JPanel(new FlowLayout());
+	        JPanel resultCPanel = new JPanel(new FlowLayout());
+	        
+	        pricePanel.setBorder(BorderFactory.createLineBorder(Color.black));
+	        resultPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+	        
+	        
 	        //JTextField textField = new JTextField("test");
 	        //frame.getContentPane().add(textField);
 	        JLabel lblPrices = new JLabel("Prices");
-	        entryContainer.setLayout(new GridLayout(4, 3));
+	        
 	        JLabel lblAstral = new JLabel("Astral");
 	        JFormattedTextField priceAstral = new JFormattedTextField();
+	        
 	        JLabel lblUnstrung = new JLabel("Unstrung");
 	        JFormattedTextField priceUnstrung = new JFormattedTextField();
+	        
 	        JLabel lblStrung = new JLabel("Strung");
 	        JFormattedTextField priceStrung = new JFormattedTextField();
-	        //ftf.validate();
-	        priceAstral.setValue("Test"); 
 	        
-	        entryContainer.add(lblPrices);
-	        entryContainer.add(new JLabel());
-	        entryContainer.add(lblAstral);
-	        entryContainer.add(priceAstral);
-	        entryContainer.add(lblUnstrung);
-	        entryContainer.add(priceUnstrung);
-	        entryContainer.add(lblStrung);
-	        entryContainer.add(priceStrung);
+	        JLabel lblcExp = new JLabel("Crafting Experience");
+	        JFormattedTextField resultCExp = new JFormattedTextField();
 	        
-	        entryContainer.add(lblResults);
-	        entryContainer.add(new JLabel());
-	        entryContainer.add(resultAstral);
-	        entryContainer.add(lblmExp);
-	        entryContainer.add(resultAstral);
-	        entryContainer.add(lblcExp); //make header labels
-	        entryContainer.add(resultAstral); //result Text field to create result need to be made
-	        entryContainer.add(lblRequest);
-	        entryContainer.add(checkRequest);
-	        entryContainer.add(priceAstral);
-	        entryContainer.add(lblUnstrung);
-	        entryContainer.add(priceUnstrung);
-	        entryContainer.add(lblStrung);
-	        entryContainer.add(priceStrung);
+	        JLabel lblmExp = new JLabel("Magic Experience");
+	        JFormattedTextField resultMExp = new JFormattedTextField();
+	    
+	        priceAstral.setValue("pAstral"); 
+	        priceUnstrung.setValue("pUnstrung"); 
+	        priceStrung.setValue("pStrung"); 
 	        
+	        
+
+	        
+	        priceAPanel.add(lblAstral);
+	        priceAPanel.add(priceAstral);        
+	        priceUPanel.add(lblUnstrung);
+	        priceUPanel.add(priceUnstrung);
+	        priceSPanel.add(lblStrung);
+	        priceSPanel.add(priceStrung);
+	        
+	        pricePanel.add(lblPrices);               
+	        pricePanel.add(lblPrices);
+	        pricePanel.add(priceAPanel);
+	        pricePanel.add(priceUPanel);
+	        pricePanel.add(priceSPanel);
+	        
+	        JLabel lblResults = new JLabel("Results");
+	        resultPanel.add(lblResults);
+	        
+	        resultPanel.add(new JLabel());
+	
+			resultMPanel.add(lblmExp);
+	        resultMPanel.add(resultMExp);
+			resultCPanel.add(lblcExp); //make header labels
+	        resultCPanel.add(resultCExp); //result Text field to create result need to be made
+	        
+	        resultPanel.add(resultCPanel);
+	        resultPanel.add(resultMPanel);
+	        
+	        entryContainer.add(pricePanel);
+	        entryContainer.add(resultPanel);
 	        
 	        //Display the window.
 	        int windowWidth = 890;
 	        int windowHeight = 500;
 	        entryContainer.setBounds(50, 100, windowWidth, windowHeight);
-	        entryContainer.pack();
+	        //entryContainer.pack();
 	        entryContainer.setVisible(true);
 	    }
 
---
+++ src/GUI/intAmuletGUI.java
@@ -0,0 +1,10 @@
+package GUI;
+
+
+public interface intAmuletGUI {
+	public void createFrame();
+	public void handleListeners();
+	public void createPricePanel();
+	public void createResultPanel();
+	public void show();
+}
